ea667bde46dac195c47610180961bd796c0ee73b
RomashkaTea <80623570+RomashkaTea@users.noreply.github.com>
2024-10-02 17:40:46 +0300

    use .unwrap() instead of .expect in some places

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use uuid::Uuid;
 use std::fs::File;
 use std::io::Write;
+use std::io::stdin;
+use std::io::stdout;
 
 const URL: &str = "https://link.brawlstars.com/?action=voucher&code=";
 
@@ -12,8 +14,8 @@ fn main() {
     let mut s = String::new();
     print!("Count: ");
 
-    std::io::stdout().flush().expect("flush failed"); // it doesn't print before read_line without flush()
-    std::io::stdin().read_line(&mut s).expect("Failed to read input");
+    stdout().flush().unwrap(); // it doesn't print before read_line without flush()
+    stdin().read_line(&mut s).unwrap();
 
     let value: i32 = s.trim().parse().expect("Your input should be a number!");
     let mut result = String::new();
@@ -24,6 +26,6 @@ fn main() {
         result.push_str(&format!("{}{}\n", URL, id));
     }
 
-    let mut file = File::create("links.txt").expect("Failed to create file links.txt!");
-    file.write_all(result.as_bytes()).expect("Failed to write file links.txt!");
+    let mut file = File::create("links.txt").unwrap();
+    file.write_all(result.as_bytes()).unwrap();
 }
